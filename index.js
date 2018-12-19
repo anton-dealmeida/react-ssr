c3cbf2032e7e7055d8c991b91697932dabb427c7
Antonio De Almeida <anton@responsive.co.za>
2018-12-19 10:11:41 +0200

    Added Application Insights SDK for Monitoring with Azure's Application Insights

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
+const appInsights = require( "applicationinsights" );
+
+appInsights.setup( "9de1df93-a5cf-45c7-9d8a-0fa0228ecd78" );
+appInsights.start();
+
 require( "babel-register" )( {
     presets: [ "env" ],
 } );
